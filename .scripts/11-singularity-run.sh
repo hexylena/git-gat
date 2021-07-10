c4373a73ac3cc69885493e6439a8972d23c3dbea
The Galaxy Training Network <galaxytrainingnetwork@gmail.com>
2021-07-10 15:55:56 +0000

    admin/init/0000: Add scripts directory

---
+++ .scripts/11-singularity-run.sh
@@ -0,0 +1,55 @@
+#!/bin/bash
+set -ex
+
+# Install dependencies before changing commits
+find .scripts -name requirements.txt | xargs --no-run-if-empty -n 1 pip install -r
+echo '[galaxyservers]' > ~/.hosts
+echo "$(hostname -f) ansible_connection=local ansible_user=$(whoami)"  >> ~/.hosts
+echo '[pulsarservers]' >> ~/.hosts
+echo "$(hostname -f) ansible_connection=local ansible_user=$(whoami)"  >> ~/.hosts
+export GALAXY_HOSTNAME="$(hostname -f)"
+export GALAXY_API_KEY=adminkey
+## The students should use a random password, we override with 'password' for reproducibility
+echo 'password' > ~/.vault-password.txt;
+## And one in this directory, it can contain garbage
+echo 'garbage' > ./.vault-password.txt;
+## Ensure the galaxy user is setup
+sudo -u galaxy /srv/galaxy/venv/bin/python /usr/bin/galaxy-create-user -c /srv/galaxy/config/galaxy.yml --user admin@example.org --password password --key adminkey --username admin
+
+# CMD
+## Checkout
+git checkout $(git log main --pretty=oneline | grep "admin/singularity/0000" | cut -c1-40)
+## Run command
+ansible-galaxy install -p roles -r requirements.yml
+
+# CMD
+## Checkout
+git checkout $(git log main --pretty=oneline | grep "admin/singularity/0003" | cut -c1-40)
+## Run command
+ansible-playbook galaxy.yml -i ~/.hosts --vault-password-file ~/.vault-password.txt
+
+# CMD
+## Checkout
+git checkout $(git log main --pretty=oneline | grep "admin/singularity/0005" | cut -c1-40)
+## Run command
+mkdir -p templates/galaxy/config
+
+# CMD
+## Checkout
+git checkout $(git log main --pretty=oneline | grep "admin/singularity/0009" | cut -c1-40)
+## Run command
+ansible-playbook galaxy.yml -i ~/.hosts --vault-password-file ~/.vault-password.txt
+
+# TEST
+## Checkout
+git checkout $(git log main --pretty=oneline | grep "admin/singularity/0009" | cut -c1-40)
+## Run test case
+./.scripts/11-singularity-test/1-run-minimap2.sh
+
+# TEST
+## Checkout
+git checkout $(git log main --pretty=oneline | grep "admin/singularity/0009" | cut -c1-40)
+## Run test case
+./.scripts/11-singularity-test/1-run-minimap2.sh
+# Done!
+git checkout main
